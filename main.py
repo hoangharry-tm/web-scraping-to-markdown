4119b8ab2aaf439aff1704302d8a3c00b794ad5a
hoangharry-tm <tonminhhoang.work@gmail.com>
2025-04-21 10:55:54 -0400

    fix: minor changes

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import os
 import re
-from typing import Literal
-from bs4 import BeautifulSoup
+from typing import Literal, Self
+
 import requests
+from bs4 import BeautifulSoup
+import ratelimit
 
 
 class DataCollector:
@@ -29,17 +31,24 @@ class DataCollector:
     def transform_html(self, content):
         return BeautifulSoup(str(content), "html.parser")
 
-    def get_data(self):
+    @ratelimit.limits(calls=20, period=60)
+    @ratelimit.sleep_and_retry
+    def get_data(self) -> Self:
         """
         Use Jina AI to get data
         """
-        for url in self.urls:
-            res = requests.get(url, timeout=10)
+        print("Getting data...")
+        os.makedirs("./cache-requests/", exist_ok=True)
+        # os.remove("./cache-requests/failed-requests.txt")
+        # os.remove("./cache-requests/success-requests.txt")
+        for i, url in enumerate(self.urls):
+            print(i + 1)
+            res = requests.get(url, timeout=1000)
             parsed_res = BeautifulSoup(res.content, "html.parser")
             md_res: list[str] = str(parsed_res).split("\n")
-            filtered_res: list[str] = \
-                list(filter(lambda x: "------" not in x and x != "",
-                            md_res))
+            filtered_res: list[str] = list(
+                filter(lambda x: "------" not in x and x != "", md_res)
+            )
 
             line: int = 0
             is_delete: bool = True
@@ -49,31 +58,52 @@ class DataCollector:
                 if "Search results updated" in filtered_res[line]:
                     is_delete = False
                 if filtered_res[line] == "1" and filtered_res[line + 1] == "2":
-                    is_delete = True # Delete all bottom parts
+                    is_delete = True  # Delete all bottom parts
                 if is_delete:
                     del filtered_res[line]
                     continue
                 line += 1
 
-            del filtered_res[0] # Delete the top line "Search results..."
+            if filtered_res == [] or is_delete is False:
+                print("Failed")
+                with open("./cache-requests/failed-requests.txt", "a") as f:
+                    f.write(f"{i}\n")
+                continue
 
-            line: int = 0
+            del filtered_res[0]  # Delete the top line "Search results..."
+
+            # for i in filtered_res:
+            #     print(i)
+
+            line = 0
+
+            log_md_link = []
 
             while line < len(filtered_res):
-                extracted_md_link: list[tuple[str, str]] = \
-                    re.findall(r"\[(.*?)\]\((.*?)\)", filtered_res[line + 1])
+                extracted_md_link: list[tuple[str, str]] = re.findall(
+                    r"\[(.*?)\]\((.*?)\)", filtered_res[line + 1]
+                )
 
-                category: str       = filtered_res[line]
-                anatomy_part: str   = extracted_md_link[0][0]
-                link: str           = extracted_md_link[0][1]
-                description: str    = filtered_res[line + 2]
+                log_md_link.append(extracted_md_link[0])
+
+                category: str = filtered_res[line]
+                anatomy_part: str = extracted_md_link[0][0]
+                link: str = extracted_md_link[0][1]
+                description: str = filtered_res[line + 2]
 
                 self.anatomy_data[anatomy_part] = {
                     "category": category,
                     "link": link,
-                    "description": description
+                    "description": description,
                 }
                 line += 3
+
+            print("Success")
+            with open("./cache-requests/success-requests.txt", "a") as f:
+                f.write(f"#{i + 1}\n")
+                for info in log_md_link:
+                    f.write(f"{info[0]}\n{info[1]}\n")
+                f.write("\n")
 
         return self
 
@@ -82,14 +112,16 @@ class DataCollector:
         Process data.
         Transform data into Markdown, dict, etc.
         """
+        print("Processing data...")
         # Traverse the dictionary
         for anatomy_part, metadata in self.anatomy_data.items():
             link = metadata["link"]
 
-            res = requests.get(link, timeout=10)
+            res = requests.get(link, timeout=1000)
             parsed_res = BeautifulSoup(res.content, "html.parser")
 
-            markdown_file = f"# {anatomy_part}\n\ncategory: {metadata['category']}\n\ndescription: {metadata['description']}\n"
+            markdown_file = f"# {anatomy_part}\n\ncategory: {
+                metadata['category']}\n\ndescription: {metadata['description']}\n"
 
             def add_to_markdown(content: str, heading_level: int = 0):
                 nonlocal markdown_file
@@ -99,7 +131,9 @@ class DataCollector:
                 markdown_file += f"\n{heading_level * '#'} {content}\n"
 
             content_signature = parsed_res.find("div", id="article-content")
-            assert content_signature is not None, "Could not find the specified element."
+            assert (
+                content_signature is not None
+            ), "Could not find the specified element."
             content_parent = content_signature.parent
             assert content_parent is not None, "Could not find the specified element."
 
@@ -108,9 +142,12 @@ class DataCollector:
                 content_structure = self.transform_html(content_structure)
                 content_structure = content_parent.find("div", id=False)
                 content_structure = self.transform_html(content_structure)
-                content_structure = content_structure.find_all(lambda tag: tag.name == "div" and tag.has_attr("id"))[0]
+                content_structure = content_structure.find_all(
+                    lambda tag: tag.name == "div" and tag.has_attr("id")
+                )[0]
                 content_structure = self.transform_html(content_structure)
                 content_structure = content_structure.find("div")
+                assert content_structure is not None
                 id = content_structure.get("id")
                 name = content_structure.find("h2", {"data-heading-level": "2"})
                 assert name is not None
@@ -118,26 +155,29 @@ class DataCollector:
                 self.content_main_headings.append((name, id))
             else:
                 content_structure = self.transform_html(content_structure)
-                content_structure = (content_structure
-                                    .find_all("span", {
-                                        "data-heading-level": "5"
-                                    }))
+                content_structure = content_structure.find_all(
+                    "span", {"data-heading-level": "5"}
+                )
 
                 for heading in content_structure:
                     heading_text = heading.text
-                    heading_href = heading.parent['href'] if heading.parent is not None else None
+                    heading_href = (
+                        heading.parent["href"] if heading.parent is not None else None
+                    )
                     assert heading_href is not None
                     heading_href = heading_href[1:]
 
                     self.content_main_headings.append((heading_text, heading_href))
 
-            print(f"{anatomy_part} >>> {self.content_main_headings}\n")
+            # print(f"{anatomy_part} >>> {self.content_main_headings}\n")
             for heading in self.content_main_headings:
                 _, id = heading
                 add_to_markdown(content=_, heading_level=2)
                 content_section = content_parent.find_all("div", {"id": id})
                 content_section = self.transform_html(content_section)
-                content_section = content_section.find("h2", {"data-heading-level": "2"})
+                content_section = content_section.find(
+                    "h2", {"data-heading-level": "2"}
+                )
                 assert content_section is not None, content_section
                 content_section = content_section.parent
                 content_section = self.transform_html(content_section)
@@ -150,8 +190,7 @@ class DataCollector:
 
         return self
 
-    def save_data(self,
-                  flag: Literal["File", "Variable"] = "File"):
+    def save_data(self, flag: Literal["File", "Variable"] = "File"):
         """
         Save data:
         - If flag is "File" -> Save markdown data to a file and return an object
@@ -168,22 +207,25 @@ class DataCollector:
             os.makedirs("./data", exist_ok=True)
             for anatomy_part, metadata in self.anatomy_data.items():
                 with open(f"./data/{anatomy_part}.md", "w") as f:
-                    f.write(metadata['markdown'])
+                    f.write(metadata["markdown"])
             return
 
 
 def main():
     Jina_AI = "r.jina.ai"
-    URLs = [(f"https://{Jina_AI}/www.elsevier.com/resources/anatomy"
-               + f"?query=&page={page_index}&sortBy=alphabeticalAsc")
-            for page_index in range(1, 2)] # FIXME: Change the range to 1 - 772
+    URLs = [
+        (
+            f"https://{Jina_AI}/www.elsevier.com/resources/anatomy"
+            + f"?query=&page={page_index}&sortBy=alphabeticalAsc"
+        )
+        # FIXME: Change the range to 1 - 772
+        for page_index in range(1, 772)
+    ]
 
     anatomy_data = DataCollector(URLs)
     # data pipeline
-    (anatomy_data
-        .get_data()
-        .process_data()
-        .save_data("File"))
+    (anatomy_data.get_data())
+        #.process_data().save_data("File"))
 
 
 if __name__ == "__main__":
